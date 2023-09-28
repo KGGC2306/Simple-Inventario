73101e20125fb7a6434114a62c3791c865c0b179
Laboratorio 109 Pc-03 <lab-109-inf-03>
2023-09-28 19:15:50 -0400

    Nueva clase agregada

--- src/SimpleInventario.java
+++ src/SimpleInventario.java
@@ -10,11 +10,24 @@ public class SimpleInventario {
     public static double obtenerPrecioArticulo (int id) {
         return 0.00;
     }
-    public static void imprimirInventario() {
+    public static String obtenerNombreArticulo (){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingrese el nombre del articulo");
+        String nombre = scanner. nextLine();
+        System.out.println("Usted ha ingresado el articulo " + nombre);
+        return nombre;
 
     }
+    public static void imprimirInventario() {
+        Scanner scanner = new Scanner(System. in);
+        System.out.println("Ingrese su nombre");
+        String usuario = scanner.nextLine();
+        System.out.println("Bienvenido " + usuario);
+        String articuloExistente = obtenerNombreArticulo();
+        System.out.println("Articulo existente " + articuloExistente);
+    }
     public static void main(String[] args) {
-        System.out.println("ingresar una opcion");
+        imprimirInventario();
     }
 }
 
